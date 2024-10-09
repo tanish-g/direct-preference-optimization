ec8b1e2e93a4e5b6a86ea731889a25810d22dfa5
Taneesh Gupta <55942819+tanish-g@users.noreply.github.com>
2024-10-09 21:55:58 +0530

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,18 +1,38 @@
 import torch
-import transformers
-if __name__=='__main__':
-  model_name_or_path = "lxuechen/phi-2-sft"
-  model= transformers.AutoModelForCausalLM.from_pretrained(
-      model_name_or_path,
-      low_cpu_mem_usage=True,
-      trust_remote_code=True,
-      torch_dtype=torch.float16,
-  )
-   
-  state_dict = torch.load("/home/azureuser/cloudfiles/code/Users/t-sshandilya/sallms/dpo_train/direct-preference-optimization/.cache/azureuser/train_phi2_dpo_scores_greater_than_3_org_1e_6_2024-09-26_15-12-27_602430/LATEST/policy.pt", map_location='cpu')
-   
-  model.load_state_dict(state_dict['state'])
-   
-  model.save_pretrained("/home/azureuser/cloudfiles/code/Users/t-sshandilya/sallms/dpo_train/direct-preference-optimization/trained_models_hf/phi-2-UF-2-epochs-scores_greater_than_3-baseline-lr_1e_6", safe_serialization=True)
- 
- 
+from transformers import AutoModelForCausalLM
+
+if __name__ == '__main__':
+    model_name_or_path = "microsoft/Phi-3-mini-4k-instruct"
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name_or_path,
+        low_cpu_mem_usage=True,
+        trust_remote_code=True,
+        torch_dtype=torch.bfloat16,
+        cache_dir='/tmp/.cache/azureuser/',
+        
+    )
+
+    load_paths = [
+        '/tmp/.cache/azureuser/train_phi3_mini_sft_random_21k_dpo_1e_6_2024-10-09_06-29-49_857828',
+        '/tmp/.cache/azureuser/train_phi3_mini_sft_baseline_dpo_1e_6_2024-10-09_08-24-18_504015',
+        '/tmp/.cache/azureuser/train_phi3_mini_sft_ours_hal_dpo_1e_6_2024-10-09_10-16-25_489121'
+    ]
+
+    save_paths = [
+        '/mnt/azureml/cr/j/694fe47564604d2bb5fa17e72155dbb1/exe/wd/outputs/train_phi3_mini_sft_random_21k_dpo_1e_6',
+        '/mnt/azureml/cr/j/694fe47564604d2bb5fa17e72155dbb1/exe/wd/outputs/train_phi3_mini_sft_baseline_dpo_1e_6',
+        '/mnt/azureml/cr/j/694fe47564604d2bb5fa17e72155dbb1/exe/wd/outputs/train_phi3_mini_sft_ours_hal_dpo_1e_6'
+    ]
+
+    for i in range(3):
+        # Load the saved state dict
+        state_dict = torch.load(f"{load_paths[i]}/LATEST/policy.pt", map_location='cpu')
+        
+        # Loading the state dict into the model
+        model.load_state_dict(state_dict['state'])
+        model.save_pretrained(
+            save_paths[i],
+            safe_serialization=True,
+        )
+        # Save the model with safe serialization enabled
+        # model.save_pretrained(f'{save_paths[i]}', safe_serialization=True)
